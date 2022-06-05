4f0409a762bb403dfa72fe90bbf6d1cb0777d6cf
[agung permana] <[agung.ten@gmail.com]>
2022-06-05 11:37:39 +0700

    fixing total harga

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const listBelanjaan = (data) => {
 const totalBelanjaan = (data) => {
   let total = 0;
   data.forEach(el => {
-    total = total + el.harga;
+    total = total + (el.harga * el.kuantitas);
   });
   return total;
 };
